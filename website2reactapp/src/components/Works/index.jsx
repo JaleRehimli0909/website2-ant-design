14dbb6b6d54cc7004695190c86c01712f9eb632b
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-11-10 22:33:58 +0400

    Works added

--- website2reactapp/src/components/Works/index.jsx
+++ website2reactapp/src/components/Works/index.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
+import { Modal } from "antd";
+import { PlayCircleOutlined } from "@ant-design/icons";
 
 const Works = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div>Works</div>
-  )
-}
+    <div className="Works">
+      <div className="container">
+        <div className="Works-content">
+          <h2>How it works</h2>
+          <p>check our latest video to know how it works</p>
+
+          <PlayCircleOutlined className="worksIcon" onClick={showModal} />
+
+          <Modal
+            title="Woocommerce Tutorial"
+            open={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={null}
+          >
+            <iframe
+              title="Woocommerce Tutorial"
+              width="100%"
+              height="350"
+              src="https://www.youtube.com/embed/8f8_JYIzOno?list=PLiUrl-SQRR7LQINGQGE99pXWDuKq4SxfU"
+            ></iframe>
+          </Modal>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Works;
